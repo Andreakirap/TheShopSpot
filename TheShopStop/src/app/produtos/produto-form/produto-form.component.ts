789820a8e295c2f9e113b24ccd23f6bb5a9c6cc5
Filipe <filipe@vighapp.com>
2021-05-20 02:32:25 -0300

    Avanço inicial The Shop Spot

--- TheShopStop/src/app/produtos/produto-form/produto-form.component.ts
+++ TheShopStop/src/app/produtos/produto-form/produto-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from '../shared/produto';
 
+
 @Component({
   selector: 'app-produto-form',
   templateUrl: './produto-form.component.html',
@@ -13,11 +14,7 @@ export class ProdutoFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.title = 'Novo Produto';
-    this.produto = new Produto();
-  }
 
-  onSubmit() {
-    console.log(this.produto);
   }
+  images = ['../../../assets/Fotos/foto.jpg', '../../../assets/Fotos/foto2.jpg', '../../../assets/Fotos/foto3.jpg'];
 }
